dfa33a3b9b34ab736119b1d4396db7a17bddd301
Adam Zimowski <mrazjava@yandex.com>
2020-03-26 08:40:58 +0100

    logout

--- src/main/java/com/github/mrazjava/booklink/service/UserService.java
+++ src/main/java/com/github/mrazjava/booklink/service/UserService.java
@@ -3,9 +3,11 @@ package com.github.mrazjava.booklink.service;
 import com.github.mrazjava.booklink.BooklinkException;
 import com.github.mrazjava.booklink.persistence.model.UserEntity;
 import com.github.mrazjava.booklink.persistence.repository.UserRepository;
+import com.github.mrazjava.booklink.security.InvalidAccessTokenException;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -78,11 +80,32 @@ public class UserService implements UserDetailsService {
         return validatedUser;
     }
 
+    public String deleteAuthToken(UserDetails credentials) {
+        Optional<UserEntity> userEntityResult = findUserByEmail(credentials.getUsername());
+        if(!userEntityResult.isPresent()) {
+            throw new UsernameNotFoundException("invalid user");
+        }
+        UserEntity userEntity = userEntityResult.get();
+        userEntity.setToken(null);
+        userEntity.setTokenExpiry(null);
+        userEntity = userRepository.save(userEntity);
+        return userEntity.getEmail();
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         Optional<UserEntity> userEntityResult = findUserByEmail(username);
         userEntityResult.orElseThrow(() -> new UsernameNotFoundException("invalid user"));
         return userEntityResult.map(Function.identity()).get();
+    }
+
+    public static UserDetails getCredentials(Authentication auth) {
+
+        if(auth == null || auth.getPrincipal() == null) {
+            throw new InvalidAccessTokenException("no user credentials");
+        }
+
+        return (UserDetails)auth.getPrincipal();
     }
 }
