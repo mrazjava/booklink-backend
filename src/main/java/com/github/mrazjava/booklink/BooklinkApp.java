f2de22e3a40da9660a7b4b48c1373ba338418fb4
Adam Zimowski <mrazjava@yandex.com>
2020-04-01 18:56:10 +0200

    debugging

--- src/main/java/com/github/mrazjava/booklink/BooklinkApp.java
+++ src/main/java/com/github/mrazjava/booklink/BooklinkApp.java
@@ -1,5 +1,7 @@
 package com.github.mrazjava.booklink;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +21,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class BooklinkApp {
 
+    private static final Logger log = LoggerFactory.getLogger(BooklinkApp.class);
+
     public static void main(String[] args) {
+
+        log.info(".... [{}]-[{}]",
+                System.getenv("AWS_ACCESS_KEY_ID"), System.getenv("AWS_SECRET_ACCESS_KEY"));
 
         new SpringApplicationBuilder()
                 .sources(BooklinkApp.class)
