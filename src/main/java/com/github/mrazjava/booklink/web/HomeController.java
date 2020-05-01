b88eca661f8be18e1f11919a538fba817cafadd5
Adam Zimowski <mrazjava@yandex.com>
2020-05-01 10:04:33 +0200

    CORS tweaks + home page

---
+++ src/main/java/com/github/mrazjava/booklink/web/HomeController.java
@@ -0,0 +1,19 @@
+package com.github.mrazjava.booklink.web;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author AZ
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String homePage() {
+        return "nothing to see here";
+    }
+}
