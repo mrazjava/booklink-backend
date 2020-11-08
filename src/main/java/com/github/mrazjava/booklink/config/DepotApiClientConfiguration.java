6926faf228b2d22aacdc799a0bff9baf96710173
Adam Zimowski <mrazjava@yandex.com>
2020-11-08 18:54:14 +0100

    exteranlize depot api config

---
+++ src/main/java/com/github/mrazjava/booklink/config/DepotApiClientConfiguration.java
@@ -0,0 +1,37 @@
+package com.github.mrazjava.booklink.config;
+
+import com.github.mrazjava.booklink.rest.depot.ApiClient;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+/**
+ * @author AZ (mrazjava)
+ * @since 0.3.0
+ */
+@Configuration
+public class DepotApiClientConfiguration {
+
+    @Inject
+    private Logger log;
+
+    @Value("${booklink.depot.url}")
+    private String depotBaseUrl;
+
+    @Autowired
+    @Qualifier("com.github.mrazjava.booklink.rest.depot.ApiClient")
+    private ApiClient apiClient;
+
+    @PostConstruct
+    void initialize() {
+        log.info(".............................................................");
+        log.info("setting base path for {} to: {}", ApiClient.class.getSimpleName(), depotBaseUrl);
+        log.info(".............................................................");
+        apiClient.setBasePath(depotBaseUrl);
+    }
+}
--- src/main/java/com/github/mrazjava/booklink/config/DepotConfiguration.java
+++ src/main/java/com/github/mrazjava/booklink/config/DepotConfiguration.java
@@ -1,22 +1,15 @@
 package com.github.mrazjava.booklink.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.github.mrazjava.booklink.rest.depot.ApiClient;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -28,23 +21,8 @@ import java.time.format.DateTimeFormatter;
 @Configuration
 public class DepotConfiguration {
 
-    @Value("${booklink.depot.url}")
-    private String depotBaseUrl;
-
     @Inject
     private Logger log;
-
-    @Autowired
-    @Qualifier("com.github.mrazjava.booklink.rest.depot.ApiClient")
-    private ApiClient apiClient;
-
-    @PostConstruct
-    void initialize() {
-        log.info(".............................................................");
-        log.info("setting base path for {} to: {}", ApiClient.class.getSimpleName(), depotBaseUrl);
-        log.info(".............................................................");
-        apiClient.setBasePath(depotBaseUrl);
-    }
 
     @Bean
     public RestTemplate provideRestTemplate(RestTemplateBuilder restTemplateBuilder) {
